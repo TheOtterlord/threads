5a5517fb905f32630f3761e9fe1340aad7c4326c
= <otterlord.dev@gmail.com>
2023-07-07 12:32:03 +0100

    Remove unneccessary request options

--- src/utils.ts
+++ src/utils.ts
@@ -4,15 +4,11 @@ export function graphql(body: string, options: { headers?: Record<string, string
       "accept": "*/*",
       "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
       "content-type": "application/x-www-form-urlencoded",
-      "sec-fetch-site": "same-origin",
       "x-ig-app-id": "238260118697367",
       ...options.headers
     },
     referrer: "https://www.threads.net",
-    referrerPolicy: "origin-when-cross-origin",
     body,
-    method: "POST",
-    mode: "cors",
-    credentials: "include"
+    method: "POST"
   })
 }
